6d6e22d875e5836ade51eb226e9f125cb9ba8987
juliantorrses@gmail.com <juliantorrses@gmail.com>
2022-11-16 09:56:58 -0600

    NUEVO

--- enviar.php
+++ enviar.php
@@ -1,41 +1,49 @@
 <html>
 <body>
- 
+
 <?php
 $C=$_POST['C'];
 $D=$_POST['D'];
-$F=0;
-$n=0;
+
+$n=$C;
 if($C>$D){
     $C+=$D;
     echo nl2br("\n\nA+B:$C" );
 }else{
-    while($n <=$D){
-        $C+=($C+1);
+    $SUMA=$C;
+    while($n <$D){
+        $C+=($SUMA+1);
         $n++;
     }
     echo nl2br("\n\nsumatoria serie de C a D:$C" );
 }
-
+$F=0;
 echo nl2br("\n\nIntereses:" );
+
 if(isset($_POST["arte"])){
         echo nl2br("\nArte");
-        $F+=10;}
+        $F=$F+10;}
+
     if(isset($_POST["danza"])){
         echo nl2br("\nDanza");
-        $F+=20;}
+        $F=$F+20;}
+
     if(isset($_POST["teatro"])){
         echo nl2br("\nTeatro");
-        $F+=40;}
+        $F=$F+40;}
+
     if(isset($_POST["baile"])){
         echo nl2br("\nBaile");
-        $F+=50;}
+        $F=$F+30;}
+
     if(isset($_POST["escultura"])){
         echo nl2br("\nEscultura");
-        $F+=50;}
+        $F=$F+50;}
+
     if(isset($_POST["astronomia"])){
         echo nl2br("\nAstronomia");
-        $F+=100;}
+        $F+=100;
+    }
 
 echo nl2br("\n\nIntereses suma:$F" );
 
