fefe4dc80dc18ce9794caa4083cd09efe0b3a823
juliantorrses@gmail.com <juliantorrses@gmail.com>
2022-11-16 09:26:16 -0600

    body

--- enviar.php
+++ enviar.php
@@ -1,3 +1,13 @@
+<html>
+<body>
+  <center>
+<body style="/* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#8fd32a+0,83db69+0,83db69+28,83db69+37,ffffff+100 */
+background: #ff9554; /* Old browsers */
+background: -moz-linear-gradient(top,  #ff2f2f 20%, #ffffff 0%, #ffffff 28%, #ffffff 37%, #6d2a21 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(top,  #ff2828 20%,#ffffff 0%,#ffffff 28%,#ffffff 37%,#6d2a21 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to bottom,  hwb(0 26% 0%) 0%,#ffffff 0%,#ffffff 28%,#ffffff 37%,#6d2a21 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8fd32a', endColorstr='#ffffff',GradientType=0 ); /* IE6-9 */
+" >
 <?php
 $A=$_POST['A'];
 $B=$_POST['B'];
@@ -31,5 +41,6 @@ echo nl2br("\n\nIntereses suma:$C" );
 echo nl2br("\n\nSexo:\n" );        
     if (isset($_POST['sexo']))
             echo $_POST['sexo'];
-
 ?>
+</body>
+</html>
